9ba09f145164b01a84999dc14ac18331099fc6d5
alebrrr <63999794+alebrrr@users.noreply.github.com>
2020-08-24 15:05:15 +0200

    Update mainwindow.py

--- mainwindow.py
+++ mainwindow.py
@@ -13,7 +13,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(871, 610)
+        MainWindow.resize(900, 800)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
